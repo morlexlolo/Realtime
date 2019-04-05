a41aa83b9c2604d235f812a215776f421dbb4a97
stepen victor <stephenvicson@gmail.com>
2019-04-05 12:06:42 +0300

    database seed successfully

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Like::class, function (Faker $faker) {
     return [
-        //
+
+        'user_id' =>function ()
+        {
+            return App\User::all()->random();
+        }
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -1,9 +1,17 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Question;
 
 $factory->define(App\Reply::class, function (Faker $faker) {
     return [
-        //
+
+        'body' => $faker->text(),
+        'question_id' =>function(){
+            return Question::all()->random();
+        },
+        'user_id' =>function(){
+            return App\User::all()->random();
+        }
     ];
 });
