ec520ece9379dd1f879f7513538b6eef2a992223
stepen victor <stephenvicson@gmail.com>
2019-04-05 13:26:55 +0300

    category crud done

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -65,7 +65,9 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+
+        return response('Update',Response::HTTP_ACCEPTED);
     }
 
     /**
